95afdf727a17ca0f9c762a9cf8a5de8c79471af1
krishnabm <krishna.bm1995@gmail.com>
2024-04-22 18:12:00 +0530

    Tape generation

--- scenes/GameBoard/game_board.gd
+++ scenes/GameBoard/game_board.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	for n in range(0,7):
+	var tape = TapeGenerator.gen_key(10,5)
+	for n in range(0,8):
 		var nextChild: PlaySlot = PlaySlot.constructor(n)
 		nextChild.global_position = (Vector2(n*100 + 250,n*100 + 250))
 		add_child(nextChild)
--- services/tape_generator.gd
+++ services/tape_generator.gd
@@ -1,7 +1,20 @@
 class_name TapeGenerator
 extends Object
 
-static func gen_key(testLength: int)-> Array[bool]:
-	randi_range(0, testLength-1)
-	return []
+static func gen_key(testLength: int, numMatches: int)-> Array[bool]:
+	if (numMatches > testLength):
+		push_error("can't do that m8")
+		
+	var ret: Array[bool] = []
+	ret.resize(testLength)
+	ret.fill(false)
+	
+	while(numMatches > 0):
+		var idx = randi_range(0, testLength-1)
+		if ret[idx]:
+			continue
+		else:
+			ret[idx] = true
+		numMatches -= 1
+	return ret
 	
