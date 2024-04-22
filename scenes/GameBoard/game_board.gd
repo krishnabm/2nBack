c627d39d99cbf2ada1cb9c0e8085d456c5d68c73
krishnabm <krishna.bm1995@gmail.com>
2024-04-22 18:12:33 +0530

    rename dynamic slots

--- scenes/GameBoard/game_board.gd
+++ scenes/GameBoard/game_board.gd
@@ -4,5 +4,6 @@ func _ready():
 	var tape = TapeGenerator.gen_key(10,5)
 	for n in range(0,8):
 		var nextChild: PlaySlot = PlaySlot.constructor(n)
+		nextChild.name = "PlaySlot" + str(n)
 		nextChild.global_position = (Vector2(n*100 + 250,n*100 + 250))
 		add_child(nextChild)
