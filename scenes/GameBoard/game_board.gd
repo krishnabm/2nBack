3d8419ee1b4fc91e00d0835908d06a949fec3368
Krishna Bahadur Mahadevan <krmahade@microsoft.com>
2024-04-22 09:46:49 +0530

    Dynamic slots

--- scenes/GameBoard/game_board.gd
+++ scenes/GameBoard/game_board.gd
@@ -1,16 +1,7 @@
 extends Node2D
 
-const play_slot_scene = preload("res://scenes/PlaySlot/play_slot.tscn")
-@onready var play_slot_0 = %PlaySlot0
-
-
 func _ready():
-	if not play_slot_scene.can_instantiate():
-		push_error("Couldn't instantiate play slot")
-
-	play_slot_0.constructor()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	for n in range(0,7):
+		var nextChild: PlaySlot = PlaySlot.constructor(n)
+		nextChild.global_position = (Vector2(n*100 + 250,n*100 + 250))
+		add_child(nextChild)
--- scenes/PlaySlot/play_slot.gd
+++ scenes/PlaySlot/play_slot.gd
@@ -5,9 +5,10 @@ const self_scene = preload("res://scenes/PlaySlot/play_slot.tscn")
 
 @export var slot_id: int
 
+func _init(id: int = 0):
+	slot_id = id
+
 static func constructor(id: int = 0)-> PlaySlot:
 	var obj = self_scene.instantiate()
-	
 	obj.slot_id = id
-	
 	return obj
