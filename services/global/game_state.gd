377d6128a1c1fe483834c6fecba3f0906045a675
krishnabm <krishna.bm1995@gmail.com>
2024-04-24 21:58:07 +0530

    Game state

---
+++ services/global/game_state.gd
@@ -0,0 +1,14 @@
+# This script is autoloaded from Project Settings > Autoload
+#
+# Global Class Name : GameState
+# This stores properties subject to frequent change during the game across multiple scenes
+extends Object
+
+var truePositiveCount: int
+var falsePositiveCount: int
+var falseNegativeCount: int
+
+func init_store(_truePositiveCount = 0, _falsePositiveCount = 0, _falseNegativeCount = 0):
+	self.truePositiveCount = _truePositiveCount
+	self.falsePositiveCount = _falsePositiveCount
+	self.falseNegativeCount = _falseNegativeCount
