26962187de2957a78c748aee24ee310d28986679
krishnabm <krishna.bm1995@gmail.com>
2024-04-28 02:55:11 +0530

    End report changes

--- scenes/GameBoard/game_board.gd
+++ scenes/GameBoard/game_board.gd
@@ -7,14 +7,14 @@ extends Node2D
 
 # Members
 var cur_tape_pointer: int
-var tape: Array[int]
-var tape_key: Array[bool]
+var primary_tape: Array[int]
+var primary_tape_key: Array[bool]
 var play_slots: Array[PlaySlot]
 var pressed_since_last_frame: bool
 
 func _ready():
-	tape_key = TapeGenerator.gen_key(GameParams.testLength,GameParams.get_test_positive_count())
-	tape = TapeGenerator.get_tape_from_key(tape_key)
+	primary_tape_key = TapeGenerator.gen_key(GameParams.testLength,GameParams.get_test_positive_count())
+	primary_tape = TapeGenerator.get_tape_from_key(primary_tape_key)
 	cur_tape_pointer = 0
 	pressed_since_last_frame = false
 	
@@ -43,14 +43,14 @@ func generate_play_slots():
 func check_on_press():
 	# cur_tape_pointer is 1 ahead of the tape index we are interested in
 	var testing_ptr = cur_tape_pointer - 1
-	if testing_ptr >= tape.size() or pressed_since_last_frame == true:
+	if testing_ptr >= primary_tape.size() or pressed_since_last_frame == true:
 		return
 	
 	pressed_since_last_frame = true
 	
 	var key_idx = testing_ptr - GameParams.nValue
 	
-	if key_idx > -1 and tape_key[key_idx] == true:
+	if key_idx > -1 and primary_tape_key[key_idx] == true:
 		GameState.truePositiveCount += 1
 	else :
 		GameState.falsePositiveCount += 1
@@ -62,24 +62,24 @@ func update_labels():
 	%FNLabel.text = %FNLabel.text.get_slice(":",0) + ": " + str(GameState.falseNegativeCount)
 # Handlers
 func _on_timer_timeout():
-	if pressed_since_last_frame == false and cur_tape_pointer <= tape.size():
+	if pressed_since_last_frame == false and cur_tape_pointer <= primary_tape.size():
 		var prev_key_idx = cur_tape_pointer - GameParams.nValue - 1
 	
-		if prev_key_idx > -1 and tape_key[prev_key_idx] == true:
+		if prev_key_idx > -1 and primary_tape_key[prev_key_idx] == true:
 			GameState.falseNegativeCount += 1
 	
-	if cur_tape_pointer == tape.size():
+	if cur_tape_pointer == primary_tape.size():
 		cur_tape_pointer += 1
 		return
 
-	if cur_tape_pointer > tape.size():
+	if cur_tape_pointer > primary_tape.size():
 		timer.stop()
 		get_tree().change_scene_to_file("res://scenes/GameReport/game_report.tscn")
 		return
 	
 	pressed_since_last_frame = false
 	
-	var slot_id: int = tape[cur_tape_pointer]
+	var slot_id: int = primary_tape[cur_tape_pointer]
 	
 	var play_slot:PlaySlot = play_slots.filter(func(slot): return slot.slot_id == slot_id)[0]
 	play_slot.do_activate()
