6520d838d382bad5984d64ffb58dd3c3d1eda6f7
krishnabm <krishnabmahadevan@gmail.com>
2024-05-16 04:15:06 +0530

    Integrate textures

--- scenes/PlaySlot/play_slot.gd
+++ scenes/PlaySlot/play_slot.gd
@@ -5,11 +5,13 @@ extends Control
 const self_scene = preload("res://scenes/PlaySlot/play_slot.tscn")
 @onready var state_chart: StateChart = %StateChart
 @onready var color_rect = %ColorRect
+@onready var default_sprite = %DefaultSprite
+@onready var audio_sprite = %AudioSprite
 
 # Members
 @export var slot_id: int
-@export_color_no_alpha var dormant_color: Color = Color(1, 1, 1)
-@export_color_no_alpha var active_color: Color = Color(0.27373531460762, 0.72879731655121, 0.71722286939621)
+var dormant_color: Color = Color(1, 1, 1)
+var active_color: Color = Color(0.27373531460762, 0.72879731655121, 0.71722286939621)
 
 static func constructor(id: int = 0)-> PlaySlot:
 	var obj = self_scene.instantiate()
@@ -19,6 +21,12 @@ static func constructor(id: int = 0)-> PlaySlot:
 func _ready():
 	if slot_id == 99:
 		color_rect.visible = false
+		return
+	
+	if GameParams.dualMode:
+		audio_sprite.visible = true
+	else:
+		default_sprite.visible = true
 
 # Commands
 func do_activate():
@@ -27,8 +35,10 @@ func do_activate():
 
 # Handlers
 func _on_active_state_entered():
-	color_rect.color = active_color
+	color_rect.visible = true
+	# color_rect.color = active_color
 
 
 func _on_active_state_exited():
-	color_rect.color = dormant_color
+	color_rect.visible = false
+	# color_rect.color = dormant_color
