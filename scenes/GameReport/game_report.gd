240f905c6c31dce592be9dd08ec57218bda9638a
krishnabm <krishnabmahadevan@gmail.com>
2024-05-17 17:38:23 +0530

    Hide secondary stats on solo mode

--- scenes/GameReport/game_report.gd
+++ scenes/GameReport/game_report.gd
@@ -1,9 +1,11 @@
 extends Node2D
+@onready var secondary_stats_container = %SecondaryStatsContainer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if not GameParams.dualMode:
+		secondary_stats_container.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
