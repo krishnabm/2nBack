56fe302269e64d5ed4b8a737fac10de201be5d9f
krishnabm <krishnabmahadevan@gmail.com>
2024-05-19 13:04:21 +0530

    Fetch stats

--- scenes/GameReport/game_report.gd
+++ scenes/GameReport/game_report.gd
@@ -1,9 +1,18 @@
 extends Node2D
-@onready var secondary_stats_container = %SecondaryStatsContainer
 
+
+#region Members
 var database: SQLite
 var dbOpsThread: Thread
+var f1: Function
+#endregion
 
+#region Nodes
+@onready var secondary_stats_container = %SecondaryStatsContainer
+@onready var chart:Chart = %Chart
+#endregion
+
+#region Methods
 func _ready():
 	if not GameParams.dualMode:
 		secondary_stats_container.visible = false
@@ -43,13 +52,23 @@ func store_stats_db():
 		"sfn": GameState.falseNegativeCount[1],
 	}
 	database.insert_row("stats", data)
+	fetch_stats_db(database)
 
+func fetch_stats_db(_database: SQLite):
+	var res = _database.select_rows("stats", "", ["*"])
+	return res
+	pass
+
+func _exit_tree():
+	if dbOpsThread.is_alive() or dbOpsThread.is_started():
+		dbOpsThread.wait_to_finish()
+#endregion
+	
+
+#region Signal handlers
 func _on_exit_button_pressed():
 	queue_free()
 	get_tree().quit()
-
-func _exit_tree():
-	dbOpsThread.wait_to_finish()
-	
 func _on_menu_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainMenu/main_menu.tscn")
+#endregion
